1bce3e3d13fe373290c419b734100e86596f66ba
Fenny Oktaviani <fotvn@gmail.com>
2024-06-01 17:20:21 +0700

    Add header porto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return <div className="text-5xl text-pink-600">Hallo gays</div>;
+  return (
+    <div className="flex w-full h-screen bg-gradient-to-r from-purple-500 to-pink-500 text-white p-8">
+      <div className="flex flex-col gap-1 bg-white w-full justify-center items-center h-[200px] rounded-md">
+        <div className="bg-gray-300 rounded-full w-16 h-16"></div>
+        <h1 className="text-black ">Fenny Oktaviani</h1>
+        <p className="text-gray-400 text-sm">QA Analys IT</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
